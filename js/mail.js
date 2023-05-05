b925e8cc70c2b38d4da2a13e141750b5ffbdebf1
HimiCos <ineewhite@gmail.com>
2023-05-05 17:43:17 +0800

    opt: mail.js combined with css to solve js miscellaneous

--- js/mail.js
+++ js/mail.js
@@ -1,34 +1,30 @@
 var outer = document.querySelector('.outer');
-var imageContainers = document.querySelectorAll('.image-container');
+const images = document.querySelectorAll('.image-container');
 var isOpen = false;
 
 outer.addEventListener('click', function () {
     if (!isOpen) {
-        for (var i = 0; i < imageContainers.length; i++) {
-            var imageContainer = imageContainers[i];
-            outer.style.height = '666px';
-            if (imageContainer.classList.contains('image1')) {
-                imageContainer.style.top = '292px';
-            } else if (imageContainer.classList.contains('image2')) {
-                imageContainer.removeAttribute('style');
-                imageContainer.style.top = '0';
-                imageContainer.style.height = '380px';
-            } else if (imageContainer.classList.contains('image3')) {
-                imageContainer.style.top = '384px';
+        outer.style.height = '666px';
+        for (let i = 0; i < images.length; i++) {
+            if (i === 0) {
+                images[i].classList.add('move');
+            } else if (i === 1) {
+                images[i].style.top = '0';
+                images[i].style.height = '380px';
+            } else if (i === 2) {
+                images[i].classList.add('move');
             }
         }
         isOpen = true;
     } else {
-        for (var i = 0; i < imageContainers.length; i++) {
-            var imageContainer = imageContainers[i];
-            outer.style.height = '320px';
-            if (imageContainer.classList.contains('image1')) {
-                imageContainer.removeAttribute('style');
-            } else if (imageContainer.classList.contains('image2')) {
-                imageContainer.removeAttribute('style');
-                imageContainer.style.transition = 'all 1.5s';
-            } else if (imageContainer.classList.contains('image3')) {
-                imageContainer.removeAttribute('style');
+        outer.style.height = '320px';
+        for (let i = 0; i < images.length; i++) {
+            if (i === 0) {
+                images[i].classList.remove('move');
+            } else if (i === 1) {
+                images[i].removeAttribute('style');
+            } else if (i === 2) {
+                images[i].classList.remove('move');
             }
         }
         isOpen = false;
