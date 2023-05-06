8d093df8b358b5c442012dcf4ae330aab4fbe655
HimiCos <ineewhite@gmail.com>
2023-05-06 18:20:43 +0800

    opt: second mail animation

--- js/mail.js
+++ js/mail.js
@@ -1,20 +1,25 @@
-var outer = document.querySelector('.outer');
-var commentHead = document.getElementById('comment-head');
-const images = document.querySelectorAll('.image-container');
-var isOpen = false;
+window.onload = function () {
+    var outer = document.querySelector('.outer');
+    var commentHead = document.getElementById('comment-head');
+    var commentBody = document.querySelector('.tk-comments');
+    const images = document.querySelectorAll('.image-container');
+    var isOpen = false;
 
-outer.addEventListener('click', function () {
+    outer.addEventListener('click', function () {
     if (!isOpen) {
-        commentHead.classList.add('move');
         images[0].classList.add('move');
         images[1].classList.add('move');
         images[2].classList.add('move');
+        commentHead.classList.add('move');
+        commentBody.classList.add('move');
         isOpen = true;
     } else {
-        commentHead.classList.remove('move');
         images[0].classList.remove('move');
         images[1].classList.remove('move');
         images[2].classList.remove('move');
+        commentHead.classList.remove('move');
+        commentBody.classList.remove('move');
         isOpen = false;
     }
 });
+};
